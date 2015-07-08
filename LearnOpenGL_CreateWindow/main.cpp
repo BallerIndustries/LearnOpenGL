678221147a149138eb2695f3fb26e3a6fac0c8fd
Angus Cheng <baller.industries@hotmail.com>
2015-07-08 13:54:17 +0800

    Finished Day 2 - Hello Triangle

--- LearnOpenGL_CreateWindow/main.cpp
+++ LearnOpenGL_CreateWindow/main.cpp
@@ -9,14 +9,55 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
+GLfloat vertices[] = 
+{
+ 	 0.5f,  0.5f, 0.0f,  // Top Right
+ 	 0.5f, -0.5f, 0.0f,  // Bottom Right
+	-0.5f, -0.5f, 0.0f,  // Bottom Left
+	-0.5f,  0.5f, 0.0f   // Top Left 
+};
+
+GLuint indices[] =
+{
+	0, 1, 3, // First Triangle
+	1, 2, 3, // Second Triangle
+};
+
+
+
+
+const char* vertexShaderSource = 
+"#version 330 core\n"
+"\n"
+"layout(location = 0) in vec3 position;\n"
+"\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\n\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"\n"
+"out vec4 color;\n"
+"\n"
+"void main()\n"
+"{\n"
+"	color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
 int main()
 {
+	// Init GLFW
 	glfwInit();
+
+	// Set all the required options for GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+	// Create a GLFWwindow object that we can use for GLFW's functions
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
 	if (window == nullptr)
 	{
@@ -26,7 +67,13 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
+
+	// Initialize GLEW to setup the OpenGL Function pointers
 	if (glewInit() != GLEW_OK)
 	{
 		printf("Failed to initialize GLEW\n");
@@ -34,40 +81,94 @@ int main()
 		return -1;
 	}
 
+	// Define the viewport dimensions
 	glViewport(0, 0, WIDTH, HEIGHT);
-	glfwSetKeyCallback(window, key_callback);
 
-	GLfloat redValue = 0.0f;
-	GLboolean goingUp = true;
+	// Compile the vertex shader.
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", infoLog);
+	}
+
+	// Compile the fragment shader
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s", infoLog);
+	}
+	
+	// Link the shader program
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// Check for linking errors.
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		printf("ERROR::PROGRAM::SHADER::LINK_FAILED\n%s", infoLog);
+	}
+	// Clean up
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// Create a VAO
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	// Create an EBO
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+	// Pass vertices to the Graphics Card.
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+
+	// Uncomment this line to enable wireframe mode.
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
-		if (goingUp)
-		{
-			redValue += 0.05f;
-
-			if (redValue >= 1.0f)
-			{
-				goingUp = GL_FALSE;
-			}
-		}
-		else
-		{
-			redValue -= 0.05f;
-
-			if (redValue <= 0.0f)
-			{
-				goingUp = GL_TRUE;
-			}
-		}
-
 		// Check and call events.
 		glfwPollEvents();
 
 		// Rending commands here
-		glClearColor(0.0f, 0.0f, redValue, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		// Swap the buffers.
 		glfwSwapBuffers(window);
