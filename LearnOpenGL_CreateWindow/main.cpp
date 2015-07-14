626d91e65793e179ed575783c9466733308373a7
Angus Cheng <baller.industries@hotmail.com>
2015-07-15 03:09:45 +0800

    Day 7 - Camera Part B

--- LearnOpenGL_CreateWindow/main.cpp
+++ LearnOpenGL_CreateWindow/main.cpp
@@ -12,8 +12,12 @@
 #include <glm/gtc/type_ptr.hpp>
 
 const GLuint WIDTH = 800, HEIGHT = 600;
+GLfloat aspect = 45.0f;
 
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void do_movement();
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 GLfloat vertices[] = 
 {
@@ -74,11 +78,13 @@ glm::vec3 cubePositions[] =
 	glm::vec3(-1.3f, 1.0f, -1.5f)
 };
 
-//GLuint indices[] =
-//{
-//	0, 1, 3, // First Triangle
-//	1, 2, 3,
-//};
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 cameraRight = glm::cross(cameraFront, cameraUp);
+
+GLfloat deltaTime = 0.0f;	// Time between current frame and last frame
+GLfloat lastFrame = 0.0f;	// Time of last frame
 
 int main()
 {
@@ -101,8 +107,13 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
+	// Hide and capture the cursor
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	// Set the required callback functions
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
@@ -141,10 +152,6 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	// Color Attribute
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	//glEnableVertexAttribArray(1);
-
 	// Texture Coordinates Attribute
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
@@ -181,31 +188,21 @@ int main()
 	glBindTexture(GL_TEXTURE_2D, 0);
 
 	glEnable(GL_DEPTH_TEST);
-
-
-
-	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-	glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
-	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-	glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
-	glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
 	
 	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
-		// Calculate the view
-		GLfloat radius = 10.f;
-		GLfloat camX = sin(glfwGetTime()) * radius;
-		GLfloat camY = cos(glfwGetTime()) * radius;
-		glm::mat4 view;
-		view = glm::lookAt(glm::vec3(camX, 0.0f, camY), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		
-		//glm::mat4 view;
-		//view = glm::lookAt()
+		// Set lastFrame and deltaTime.
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 
+		glm::mat4 view;
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+		
 		// Check and call events.
 		glfwPollEvents();
+		do_movement();
 
 		// Clear the color buffer
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -224,7 +221,7 @@ int main()
 		glUniform1i(glGetUniformLocation(shader.Program, "ourTexture2"), 1);
 
 		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(45.0f), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+		projection = glm::perspective(aspect, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
 
 		// Send matrices to the GPU
 		GLint viewLoc = glGetUniformLocation(shader.Program, "view");
@@ -258,6 +255,71 @@ int main()
 	return 0;
 }
 
+bool keys[1024];
+
+void do_movement()
+{
+	// Camera controls
+	GLfloat cameraSpeed = 5.0f * deltaTime;
+
+	if (keys[GLFW_KEY_W])
+		cameraPos += cameraSpeed * cameraFront;
+
+	if (keys[GLFW_KEY_S])
+		cameraPos -= cameraSpeed * cameraFront;
+
+	if (keys[GLFW_KEY_A])
+		cameraPos -= cameraSpeed * cameraRight;
+
+	if (keys[GLFW_KEY_D])
+		cameraPos += cameraSpeed * cameraRight;
+}
+
+GLfloat lastX = 400, lastY = 300;
+GLfloat yaw, pitch;
+bool firstMouse = true;
+
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (aspect >= 1.0f && aspect <= 45.0f)
+		aspect -= yoffset;
+
+	aspect = glm::clamp(aspect, 1.0f, 45.0f);
+}
+
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	GLfloat sensitivity = 0.05;
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	pitch = glm::clamp(pitch, -89.0f, 89.0f);
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+}
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	// When a user presses the escape key, we set the WindowShouldClose property to true, 
@@ -266,4 +328,23 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
+
+	if (action == GLFW_PRESS || action == GLFW_REPEAT)
+		keys[key] = true;
+	else
+		keys[key] = false;
+
+	//GLfloat cameraSpeed = 0.15f;
+
+	//if (key == GLFW_KEY_W)
+	//	cameraPos += cameraSpeed * cameraFront;
+
+	//if (key == GLFW_KEY_S)
+	//	cameraPos -= cameraSpeed * cameraFront;
+
+	//if (key == GLFW_KEY_A)
+	//	cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+	//if (key == GLFW_KEY_D)
+	//	cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
